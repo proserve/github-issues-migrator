2a4779b1e77f7535ed3acd4a0ce734ed5256854b
tarrsalah <tarrsalah@gmail.com>
2016-06-14 23:59:39 +0100

    Add the outhlib dependency to setup.py.

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='FlaskApp',
       author='Ryan Jarvinen',
       author_email='ryanj@redhat.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask>=0.10.1', 'python-dateutil', 'httplib2'],
+      install_requires=['Flask>=0.10.1', 'python-dateutil', 'httplib2', 'oauthlib'],
       )
